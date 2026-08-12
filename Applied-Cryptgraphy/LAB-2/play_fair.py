b8b571930899e6f1cfcfb25888c8ff6a92f9469c
KDhyanaSamaga <kdhyanasamaga@gmail.com>
2026-08-12 07:05:56 +0530

    play fair cipher

--- Applied-Cryptgraphy/LAB-2/play_fair.py
+++ Applied-Cryptgraphy/LAB-2/play_fair.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def playFair_cipher_encryption(plain_text,key):
     cipher_text = ""
     plain_text = plain_text.lower()
@@ -6,6 +8,17 @@ def playFair_cipher_encryption(plain_text,key):
     matrix = []
     for char in key:
         matrix.append(char)
+
+    # Remove J Since we dont use that much
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    for char in matrix:
+        if char not in matrix:
+            matrix.append(char)
+
+    matrix = np.array(matrix).reshape(5,5)
+    
+
+
 
 def playFair_cipher():
     plain_text = input("Enter the plain text: ")
